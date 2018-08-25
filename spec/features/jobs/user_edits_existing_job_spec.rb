f29a3dc980df85d3e8e0cc4f0e02cb48dffc472d
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-25 17:06:44 -0600

    WIP resolve error - unable to find visible select box that is not disabled

--- spec/features/jobs/user_edits_existing_job_spec.rb
+++ spec/features/jobs/user_edits_existing_job_spec.rb
@@ -4,14 +4,14 @@ describe "User edits an existing job" do
   scenario "a user can edit a job" do
     company = Company.create(name: 'Turing')
     category = Category.create!(title: "Crypto Dev")
-    job = company.jobs.create!(title: 'Software Dev', level_of_interest: 30, city: "Denver", category_id: category.id)
+    job = company.jobs.create!(title: 'Software Dev', level_of_interest: 3, city: "Denver", category_id: category.id)
     # job.category_id.title = "Crypto Dev"
     visit company_path(company)
     click_link "Edit"
 
     expect(current_path).to eq(edit_company_job_path(company, job))
     # visit edit_company_job_path(company, job)
-
+    # select "job[category]", from: 'category-dropdown'
     fill_in "job[title]", with: "Chef"
     select "job[level_of_interest]", from: 'interest-dropdown'
     fill_in "job[city]", with: "Austin"
