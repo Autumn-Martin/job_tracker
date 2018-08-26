a61dccb87fe2e613e068a72a04ffa7f805f5d87e
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-26 17:36:04 -0600

    resolve spec error - Couldn't find Job without an ID

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -19,7 +19,7 @@ class CompaniesController < ApplicationController
 
   def show
     @company = Company.find(params[:id])
-    @job = @company.jobs.find(params[:id])
+    @job = @company.jobs.all
   end
 
   def edit
