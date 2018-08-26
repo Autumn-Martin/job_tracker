ef5b96de8722ccf4aee84a822e079c077f851974
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-26 17:25:02 -0600

    resolve companies controller route error

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -15,16 +15,16 @@ require 'rails_helper'
 # end
 
 describe 'user deletes a job' do
-  describe 'they link form the job index page' do
+  describe 'they link from the job index page' do
     it 'displays all jobs without the deleted entry' do
       company = Company.create!(name: "Nintendo")
       job = company.jobs.create!(title: "Graphic Designer", level_of_interest: 70, city: "Denver")
 
-      visit company_jobs_path(company, job)
-      save_and_open_page
+      visit jobs_path(job)
+
       click_link "Delete"
 
-      expect(current_path).to eq(company_job_path(company, job))
+      expect(current_path).to eq(jobs_path(job))
       expect(page).to have_content("Nintendo position was successfully deleted!")
       expect(page).to_not have_content(job.title)
 
