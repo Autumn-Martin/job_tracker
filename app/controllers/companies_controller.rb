9bb99d23c9a485a9d3e3abf8e1849797e47c24c0
Nick Lindeberg <lindebergnick@gmail.com>
2018-08-27 11:32:25 -0600

    updates paths

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -16,7 +16,7 @@ class CompaniesController < ApplicationController
     @company = Company.new(company_params)
     if @company.save
       flash[:success] = "#{@company.name} added!"
-      redirect_to company_path(@company)
+      redirect_to companies_path
     else
       render :new
     end
@@ -36,7 +36,7 @@ class CompaniesController < ApplicationController
     @company.update(company_params)
     if @company.save
       flash[:success] = "#{@company.name} updated!"
-      redirect_to company_path(@company)
+      redirect_to companies_path
     else
       render :edit
     end
--- spec/features/companies/user_edits_existing_company_spec.rb
+++ spec/features/companies/user_edits_existing_company_spec.rb
@@ -5,11 +5,11 @@ describe "User edits an existing company" do
     company = Company.create!(name: "ESPN")
     job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver", company_id: company.id)
     visit edit_company_path(company)
-    # save_and_open_page
+
     fill_in "company[name]", with: "EA Sports"
     click_button "Update"
 
-    expect(current_path).to eq("/companies/#{Company.last.id}")
+    expect(current_path).to eq(companies_path)
     expect(page).to have_content("EA Sports")
     expect(page).to_not have_content("ESPN")
   end
