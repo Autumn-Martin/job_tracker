139ef1326cb38f7df4b9b7d10e45f2220d332b20
Nick Lindeberg <lindebergnick@gmail.com>
2018-08-28 11:50:45 -0600

    TESTS that no duplicate category can be created

--- spec/features/categories/user_creates_new_category_spec.rb
+++ spec/features/categories/user_creates_new_category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "User creates a new category" do
   scenario "a user can create a new category" do
-    
+
     visit categories_path
 
     click_link 'Create New Category'
@@ -12,5 +12,24 @@ describe "User creates a new category" do
 
     expect(current_path).to eq(categories_path)
     expect(page).to have_content("Development")
+  end
+
+  it 'will not let a user create a duplicate category' do
+
+    visit categories_path
+    click_link 'Create New Category'
+
+    fill_in "category[title]", with: "Development"
+    click_button "Create"
+
+    visit categories_path
+
+    click_link 'Create New Category'
+
+    fill_in "category[title]", with: "Development"
+    click_button "Create"
+
+    expect(current_path).to eq(new_category_path)
+    expect(page).to have_content("Category already exists!")
   end
 end
