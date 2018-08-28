02f48586a268ad8d27ca02fc19c23448530584b7
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-27 20:30:44 -0600

    adjust feature test for comments

--- spec/features/comments/user_sees_all_comments_spec.rb
+++ spec/features/comments/user_sees_all_comments_spec.rb
@@ -4,14 +4,14 @@ describe 'User sees all comments for a job' do
   describe 'when user visits job/show page' do
     it 'shows all comments for a job' do
       company = Company.create(name: 'NASA')
-      job = company.jobs.create!(title: "Astronaut", level_of_interest: 80, city: "Denver", category_id: category.id)
+      job = company.jobs.create!(title: "Astronaut", level_of_interest: 80, city: "Denver")
       comment_1 = job.comments.create!(content: "Let's go to mars!")
       comment_2 = job.comments.create!(content: "I wonder what Jupiter is like.")
 
       visit job_path(job)
 
-      expect(page).to_have content(comment_1)
-      expect(page).to_have content(comment_2)
+      expect(page).to have_content(comment_1)
+      expect(page).to have_content(comment_2)
     end
   end
 end
