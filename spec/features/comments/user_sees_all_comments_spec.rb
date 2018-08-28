9ce9ded1c7ffa69b522f099240000d610647c479
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-08-27 20:22:17 -0600

    add feature test for if user sees all comments

---
+++ spec/features/comments/user_sees_all_comments_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'User sees all comments for a job' do
+  describe 'when user visits job/show page' do
+    it 'shows all comments for a job' do
+      company = Company.create(name: 'NASA')
+      job = company.jobs.create!(title: "Astronaut", level_of_interest: 80, city: "Denver", category_id: category.id)
+      comment_1 = job.comments.create!(content: "Let's go to mars!")
+      comment_2 = job.comments.create!(content: "I wonder what Jupiter is like.")
+
+      visit job_path(job)
+
+      expect(page).to_have content(comment_1)
+      expect(page).to_have content(comment_2)
+    end
+  end
+end
